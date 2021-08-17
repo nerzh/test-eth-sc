a1eeb74ceb48783545e03b4bd0bb419e16064a78
Oleh Hudeichuk <emptystamp@gmail.com>
2021-08-17 17:40:57 +0300

    fix vesting + tests

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.6;
+pragma solidity 0.8.7;
 
 struct VestingAccount {
     uint vestingStart;
-    uint vestingDays;
+    uint parts;
+    uint accuracyInSeconds;
     uint amount;
     uint withdrawn;
 }
@@ -11,8 +12,6 @@ struct VestingAccount {
 contract Vesting {
     mapping(address => VestingAccount) public accounts;
     address public owner = msg.sender;
-    uint public test1;
-    address public test2;
     
     modifier onlyOwner() {
         require(
@@ -22,49 +21,35 @@ contract Vesting {
         _; // сюда будет вставлено само тело функции
     }
   
-    function setVesting(address account, uint amount, uint vestingDays) public payable onlyOwner {
-        require(accounts[account].withdrawn == accounts[account].vestingDays, "Vesting exists");
+    function setVesting(address account, uint amount, uint parts, uint accuracyInSeconds) public payable onlyOwner {
+        VestingAccount memory memoryAccount = accounts[account];
+        require(memoryAccount.withdrawn == memoryAccount.parts, "Vesting exists");
         require(msg.value == amount, "Vesting amount should equal to value");
-        require(vestingDays > 0, "Vesting termin should be more then zero");
-        require(amount >= vestingDays, "Very small value");
+        require(parts > 0, "Vesting termin should be more then zero");
+        require(amount >= parts, "Very small value");
         require(account != 0x0000000000000000000000000000000000000000, "Account should be not 0x0000000000000000000000000000000000000000");
         accounts[account].amount = amount;
         accounts[account].vestingStart = block.timestamp;
-        accounts[account].vestingDays = vestingDays;
+        accounts[account].parts = parts;
+        accounts[account].accuracyInSeconds = accuracyInSeconds;
         accounts[account].withdrawn = 0;
     }
-  
-    struct Test {
-        uint amountPart;
-        uint timeDifferent;
-        uint availableParts;
-        uint transferAmount;
-        uint balance;
-    } 
     
-    Test public test;
     function claim() public {
-        // emit UInt(address(this).balance);
         VestingAccount memory account = accounts[msg.sender];
         require(account.vestingStart != 0, "Account Not Found");
-        uint amountPart = account.amount / account.vestingDays;
-        test.amountPart = amountPart;
-        uint timeDifferent = (block.timestamp - account.vestingStart) / 30 seconds;
-        test.timeDifferent = timeDifferent;
+        uint amountPart = account.amount / account.parts;
+        uint timeDifferent = (block.timestamp - account.vestingStart) / account.accuracyInSeconds;
         require(timeDifferent > account.withdrawn, "Nothing to withdraw");
         uint availableParts = timeDifferent - account.withdrawn;
-        test.availableParts = availableParts;
         uint transferAmount;
-        if (availableParts + account.withdrawn == account.vestingDays) {
+        if (availableParts + account.withdrawn == account.parts) {
             transferAmount = account.amount - account.withdrawn * amountPart;
         } else {
             transferAmount = availableParts * amountPart;
         }
-        test.transferAmount = transferAmount;
         require(transferAmount != 0, "Nothing to withdraw");
         payable(msg.sender).transfer(transferAmount);
         accounts[msg.sender].withdrawn = account.withdrawn + availableParts;
-        test.balance = address(this).balance;
-        // 7000000000000000000
     }
 }
--- test/vestingTest.js
+++ test/vestingTest.js
@@ -1,12 +1,101 @@
 const Web3 = require('web3');
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
-const { expect } = require('chai');
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
+
 const BigNumber = require('bignumber.js');
 const {constants} = require('openzeppelin-test-helpers');
 const { ethers } = require("ethers");
 const truffleAssert = require('truffle-assertions');
+const timeMachine = require("ganache-time-traveler");
 
 /*eslint-disable no-undef*/
 const Vesting = artifacts.require('Vesting');
 
 BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
+
+describe("TestSet for vesting contract", () => {
+  let deployer, user1, user2;
+
+  let vesting;
+  let snapshotId;
+
+  before(async () => {
+      [deployer, user1, user2] = await web3.eth.getAccounts();
+
+      console.log(user1, user2);
+      // token = await Vesting.new({ from: deployer });
+      vesting = await Vesting.new();
+  });
+  
+  describe("Simple test", () => {
+      beforeEach(async () => {
+          // Create a snapshot
+          const snapshot = await timeMachine.takeSnapshot();
+          snapshotId = snapshot["result"];
+          // Deploy contract to ganache
+          vesting = await Vesting.new();
+      });
+
+      afterEach(async () => await timeMachine.revertToSnapshot(snapshotId));
+
+      it("Test set data", async () => {
+        let amount = BigNumber(7 * 10**18);
+        await vesting.setVesting(
+            user1, 
+            amount, 
+            3, 
+            5 * 60 * 1000,
+            {from: deployer, value: amount}
+        );
+        let account = await vesting.accounts.call(user1);
+        assert.isAtMost(Number(account.vestingStart), Number(Math.floor(Date.now() / 1000)));
+        expect(String(account.amount)).to.equal(String(7 * 10**18));
+        expect(String(account.parts)).to.equal(String(3));
+        expect(String(account.accuracyInSeconds)).to.equal(String(5 * 60 * 1000));
+        expect(String(account.withdrawn)).to.equal(String(0));
+      });
+
+      it("Test only Owner can set vesting", async () => {
+        let error = 'This function is restricted to the contract\'s owner'
+        await vesting.setVesting(
+            user1,
+            1, 
+            1, 
+            1,
+            {from: user1, value: 1}
+        ).should.be.rejectedWith(`Returned error: VM Exception while processing transaction: revert ${error} -- Reason given: ${error}.`);
+      });
+
+      it("Test vesting exist", async () => {
+        let amount = BigNumber(7 * 10**18);
+        let vesting = await Vesting.new();
+        await vesting.setVesting(
+            user1, 
+            amount, 
+            3, 
+            5 * 60 * 1000,
+            {from: deployer, value: amount}
+        );
+        let error = 'Vesting exists';
+        await vesting.setVesting(
+            user1, 
+            amount, 
+            3, 
+            5 * 60 * 1000,
+            {from: deployer, value: amount}
+        ).should.be.rejectedWith(`Returned error: VM Exception while processing transaction: revert ${error} -- Reason given: ${error}.`);
+      });
+  });
+});
+
+
+
+
+
+
